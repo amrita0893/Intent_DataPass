5bcc1b5cdbce26d729c3b729c3bd5e3082178dd5
amrita <dorapanwar08@gmail.com>
2018-07-08 04:03:35 +0530

    Initial commit

---
+++ app/src/main/java/com/example/dell/intent/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.dell.intent;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText text;
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        text = findViewById(R.id.edit_text1);
+        btn = findViewById(R.id.next);
+
+        btn.setOnClickListener(new View.
+                OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = text.getText().toString();
+                Intent intent = new Intent(
+                        MainActivity.this,
+                        SecondActivity.class);
+                intent.putExtra("Name", name);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/dell/intent/SecondActivity.java
@@ -0,0 +1,25 @@
+package com.example.dell.intent;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_activity);
+
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("Name");
+
+        TextView textView;
+        textView = findViewById(R.id.text_view);
+
+        textView.setText("Hello " + name);
+    }
+}
+
